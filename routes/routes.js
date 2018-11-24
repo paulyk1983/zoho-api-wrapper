f60af639e625ea8ddb147fc17d7447f6ef2685e1
Paul <paulyk1983@gmail.com>
2018-11-24 16:14:15 -0600

    commented out everything except bare minimum to get working in p

--- routes/routes.js
+++ routes/routes.js
@@ -6,15 +6,15 @@ var appRouter = function(app) {
       res.send("Hello World");
   });
 
-  app.all('/pid', (req, res) => {
-    res.end(`process ${process.pid} says hello!`);
-  });
+  // app.all('/pid', (req, res) => {
+  //   res.end(`process ${process.pid} says hello!`);
+  // });
 
-  app.get('/locations', (req,res) => {
-    const data = fs.readFileSync('mocks/store-locator.json', 'utf8');
-    const contents = JSON.parse(data);
-    res.json(contents).status(200);
-  });
+  // app.get('/locations', (req,res) => {
+  //   const data = fs.readFileSync('mocks/store-locator.json', 'utf8');
+  //   const contents = JSON.parse(data);
+  //   res.json(contents).status(200);
+  // });
 }
 
 module.exports = appRouter;
