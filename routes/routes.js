78f31fc4c975524f7e63476a5f8f84fc2ad7e682
paulyk1983 <paulyk1983@gmail.com>
2019-01-19 15:17:53 -0600

    refactored some for the leads routes and had it return a json payload (leads)

--- routes/routes.js
+++ routes/routes.js
@@ -7,36 +7,41 @@ const axios = require('axios');
 
 var appRouter = function(app) {
 
+  var getAccessToken = function() {
+    const url = `https://accounts.zoho.com/oauth/v2/token?refresh_token=${process.env.REFRESH_TOKEN}&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&grant_type=refresh_token`;
+
+    return axios.post(url);
+  }
+
+
   app.get("/", function(req, res) {
       res.send("Hello World");
   });
 
-  var getAccessToken = function() {
-    const url = `https://accounts.zoho.com/oauth/v2/token?refresh_token=${process.env.REFRESH_TOKEN}&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&grant_type=refresh_token`;
-    
-    return axios.post(url);
-  }
-
   app.get('/locations', function(req,res) {
     const data = fs.readFileSync('mocks/store-locator.json', 'utf8');
     const contents = JSON.parse(data);
+    
     res.json(contents).status(200);
   });
 
-  app.get('/leads', function(res,req) {
-    // get access token
+  app.get('/leads', function(req,res) {
+
+    var getLeads = function(token) {
+      return axios.get('https://www.zohoapis.com/crm/v2/Leads', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+    }
+
     getAccessToken()
     .then(function (response) {
       const access_token = response.data.access_token;
       
-      // get leads
-      axios.get('https://www.zohoapis.com/crm/v2/Leads', {
-        headers: {
-          Authorization: `Bearer ${access_token}`
-        }
-      })
+      getLeads(access_token)
       .then(function (response) {
-        console.log(response);
+        res.json(response.data).status(200);       
       })
       .catch(function (error) {
         console.log(error);
@@ -46,7 +51,7 @@ var appRouter = function(app) {
     .catch(function (error) {
       console.log(error);
     });
-
+    
   });
 }
 
