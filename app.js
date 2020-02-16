2c6b89b63b448e1dc1bf62577c4ea1574f4ee252
Paul Krajewski <paulkrajewski@Pauls-MacBook-Pro.local>
2020-02-16 17:07:06 -0600

    quick cleanup

--- app.js
+++ app.js
@@ -2,31 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
-// const logger = require('./config/winston');
-// Set up mongoose connection
-// if (process.env.NODE_ENV !== 'production') {
-//    eslint-disable global-require 
-//   require('dotenv').load();
-//   /* eslint-enable global-require */
-// }
+
 const app = express();
 app.use(bodyParser.json({ extended: false }));
 var routes = require("./routes/routes.js")(app);
 const port = process.env.PORT || 1337;
-
-
-// app.use(require('morgan')('combined', { stream: logger.stream }));
-
-
-// app.use((err, req, res, next) => {
-//   logger.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
-
-//   res.status(err.status || 500);
-
-//   res.render('error');
-// });
-
-
 
 app.listen(port);
 
